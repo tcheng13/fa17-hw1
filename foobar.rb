32cd105034c4cb92079d015c909a32c5de3f2015
Tim Cheng <tcheng13@berkeley.edu>
2017-09-19 19:39:02 -0700

    hw1 commit

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,13 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+
+    a = a.map{|x| x.to_i}
+    a = a.map{|x| x + 2}
+    a = a.uniq
+    a = a.reject{|x| x > 10}
+    a = a.select{|x| x % 2 == 0}
+    return a.reduce(:+)
+
   end
 end
-
-
